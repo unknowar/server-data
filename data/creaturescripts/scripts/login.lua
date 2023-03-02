a6f1357c4ec6c13a6eb2b9d8e83a3ce1a240adc6
Siewdass <siewdass@gmail.com>
2023-03-01 20:34:46 -0400

    Uploading data

---
+++ data/creaturescripts/scripts/login.lua
@@ -0,0 +1,39 @@
+function onLogin(player)
+	local pos = player:getPosition()
+	local colors = {5,30,35,95,108,129,143,155,180,198,210,215}
+	for i = 1, #colors do
+			addEvent(function(x, c)
+					Game.sendAnimatedText('this is where i spawned', x, c)
+			end, i * 1000, pos, colors[i])
+	end
+	local serverName = configManager.getString(configKeys.SERVER_NAME)
+	local loginStr = "Welcome to " .. serverName .. "!"
+	if player:getLastLoginSaved() <= 0 then
+		loginStr = loginStr .. " Please choose your outfit."
+		player:sendOutfitWindow()
+	else
+		if loginStr ~= "" then
+			player:sendTextMessage(MESSAGE_STATUS_DEFAULT, loginStr)
+		end
+
+		loginStr = string.format("Your last visit in %s: %s.", serverName, os.date("%d %b %Y %X", player:getLastLoginSaved()))
+	end
+	player:sendTextMessage(MESSAGE_STATUS_DEFAULT, loginStr)
+
+	-- Promotion
+	local vocation = player:getVocation()
+	local promotion = vocation:getPromotion()
+	if player:isPremium() then
+		local value = player:getStorageValue(PlayerStorageKeys.promotion)
+		if value == 1 then
+			player:setVocation(promotion)
+		end
+	elseif not promotion then
+		player:setVocation(vocation:getDemotion())
+	end
+
+	-- Events
+	player:registerEvent("PlayerDeath")
+	player:registerEvent("DropLoot")
+	return true
+end
